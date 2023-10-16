1605be17586051fed3be88ae3c5bd9a038ed129f
Simon Olsen <simonrolsen@gmail.com>
2023-10-17 00:19:37 +0200

    Player Interface

--- cdio2/src/main/java/com/g16/game/Player.java
+++ cdio2/src/main/java/com/g16/game/Player.java
@@ -1,5 +1,11 @@
 package com.g16.game;
 
+interface player{
+    public int Account;
+    public String name;
+    public String setName():
+}
+
 public class Player {
     private String name;
     public Player (String _name) {
