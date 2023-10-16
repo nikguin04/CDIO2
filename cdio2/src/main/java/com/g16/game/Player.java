d1852055d5c01c992b56502899980bf96e0d80d8
Simon Olsen <simonrolsen@gmail.com>
2023-10-17 00:26:28 +0200

    Bugfixes

--- cdio2/src/main/java/com/g16/game/Player.java
+++ cdio2/src/main/java/com/g16/game/Player.java
@@ -1,18 +1,21 @@
 package com.g16.game;
 
 interface player{
-    public int account;
-    public String name;
+    public int setAccount();
     public String setName();
 }
 
 class Player implements player{
-
+    public String name;
+    public int account;
     public Player(){
         this.name = name;
         this.account = account;
     }
-    public String setname(){
+    public String setName(){
         return this.name;
+    }
+    public int setAccount(){
+        return this.account;
     }
 }
