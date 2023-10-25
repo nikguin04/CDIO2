4c4b8f0748c9c3dd67ad9ee966772fb56bbb6898
Emil Pedersen <33371016+SMan0103@users.noreply.github.com>
2023-10-25 10:32:46 +0200

    Update Account.java
    Money can't be lower then 0

--- cdio2/src/main/java/com/g16/game/Account.java
+++ cdio2/src/main/java/com/g16/game/Account.java
@@ -7,6 +7,8 @@ public class Account{
     }
     public void ModifyMoney(int _money) {
         Money += _money;
+        if(Money < 0) 
+            Money = 0;
     }
 
     public Account(int startMoney) {
